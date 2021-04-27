028a21b5d277d61c52f2cf7bb6b6b9a318f22dd6
CaoCaiTao <c0202@vip.qq.com>
2021-04-27 09:10:12 +0800

    首页

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,116 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <el-container class="height">
+      <el-header id="header">
+        <el-row>
+          <el-col :span="12">
+            <img style="height: 55px" src="./img/logo.png">
+          </el-col>
+          <el-col :span="12" id="div1">
+            <a href="#">你好,请登录</a>
+            <a href="#">注册</a>
+            <a href="#"><i class="el-icon-user"></i>我的订单</a>
+            <a href="#"><i class="el-icon-shopping-cart-2"></i>购物车</a>
+            <a href="#">商品分类</a>
+          </el-col>
+        </el-row>
+        <hr/>
+      </el-header>
+      <el-main>
+        <div id="div-cx" style="margin-top: 15px; width: 700px">
+          <el-input placeholder="请输入内容" v-model="input3" class="input-with-select">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </div>
+        <div>
+          <slot>
+            <!--Carousel 走马灯-卡片话-->
+            <el-carousel :interval="4000" type="card" height="300px">
+              <el-carousel-item v-for="item in 5" :key="item">
+                <h3 class="medium">{{ item }}</h3>
+              </el-carousel-item>
+            </el-carousel>
+          </slot>
+        </div>
+        <div style="width: 20px;"></div>
+        <el-menu style="padding-left: 150px;padding-right: 150px" router :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+          <el-menu-item index="/Hot"><span style="font-size: 20px">热销</span></el-menu-item>
+          <el-menu-item index="/Other">其他1</el-menu-item>
+        </el-menu>
+        <router-view></router-view>
+        <div style="height: 600px">
+
+        </div>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+html,body,#app{
+  margin: 0px;
+  height: 100%;
+}
+.height{
+  height: 100%;
+}
+#header{
+
+}
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
 }
 
-h1, h2 {
-  font-weight: normal;
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+.el-carousel__item:nth-child(2n+1) {
+  background-color: #d3dce6;
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
 }
 
-a {
-  color: #42b983;
+.bg-purple {
+  background: #d3dce6;
 }
+
+.bg-purple-light {
+  background: #e5e9f2;
+}
+
+el-header span,a{
+  padding-right: 20px;
+  color: #99a9bf;
+  font-size: 20px;
+  text-decoration: none;
+}
+
+#div1{
+  text-align:center;
+  margin:20px auto;
+}
+
+#div1 a:hover {  color : red ; }
+
+#div-cx{
+  text-align:center;
+  margin:20px auto;
+}
+
+
 </style>
---
+++ src/router/Hot.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="divcss5">
+    <div style="padding-left: 150px;padding-right: 150px">
+      <el-row>
+        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+      </el-row>
+      <el-row style="padding-top: 5px">
+        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+      </el-row>
+      <el-row style="padding-top: 5px">
+        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+      </el-row>
+      <el-row style="padding-top: 5px">
+        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+      </el-row>
+      <el-row style="padding-top: 5px">
+        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hot",
+  data(){
+    return{
+      msg:"热门商品"
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 200px;
+}
+
+.divcss5{
+  width:100%;
+  height: 100%;
+  padding-top: 20px;
+}
+
+.el-col{
+  padding-right: 5px
+}
+</style>
