2b12b803fa8aea484119595036c3cdce34005c31
CaoCaiTao <c0202@vip.qq.com>
2021-04-27 09:11:49 +0800

    首页

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+//引用router.js
+import router from './router.js'
+Vue.config.productionTip = false
 
 Vue.use(ElementUI)
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
