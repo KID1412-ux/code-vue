11263f7be7f80838feedfec1880225aaa5ebbf33
yanhan-maker <424133659@qq.com>
2021-04-28 08:46:24 +0800

    后台登录页面

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App5 from './App5.vue';
+import router from './router';
+
+
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+
+import Axios from "axios";
+
+Axios.defaults.baseURL="http://127.0.0.1:8090/code"
+
+Vue.prototype.$axios = Axios
+
+Vue.use(ElementUI)
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App5),
+  router
 })
