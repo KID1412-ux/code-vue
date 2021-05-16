4bc32e676c15b4f112d74481f1a7fc5f12a67d60
KID1412-ux <xiangtanliuyu@126.com>
2021-05-16 14:49:09 +0800

    Commit Message

--- src/components/shop_cart/shop_cart.vue
+++ src/components/shop_cart/shop_cart.vue
@@ -425,6 +425,7 @@ export default {
         params.append("merchantId", _this.submitForm.merchantId);
         params.append("amount", _this.summation);
         params.append("stats", "0");
+        params.append("userId", _this.userId);
         return _this.$axios.post("shopCart/saveMerchantOrder", params);
       }
 
